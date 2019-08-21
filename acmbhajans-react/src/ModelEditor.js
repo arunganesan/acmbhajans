7b11b2cc77034c29acdbc3c55480d132309869d8
Arun Ganesan <arunganesan123@gmail.com>
2019-08-21 07:08:33 -0400

    added new model editor file

---
+++ acmbhajans-react/src/ModelEditor.js
@@ -0,0 +1,129 @@
+import React from "react";
+import 'react-virtualized/styles.css'
+import { AutoSizer, List } from 'react-virtualized'
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button, Modal, Form } from 'react-bootstrap';
+
+export class ModelEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            form: props.initForm(),
+            rowCount: 0,
+            overscanRowCount: 100,
+            showEditForm: false,
+        };
+        
+        this.renderRow = this.renderRow.bind(this);
+        this.generateForm = this.generateForm.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    generateForm() {
+      let onHide = () => this.setState({ showEditForm: false })
+      
+     
+      return (<Modal
+        show={this.state.showEditForm}
+        onHide={onHide}
+        {...this.props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Form onSubmit={this.handleSubmit}>
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              {this.props.pageName}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {this.props.editForm}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button type="submit">Submit form</Button>
+            <Button onClick={onHide}>Close</Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>);
+    }
+
+    
+    componentDidMount() {
+        fetch(this.props.URL)
+        .then(res => res.json())
+        .then(data => {
+            this.setState({
+                ...data
+            })
+        });
+    }
+
+
+    handleSubmit(event) {
+      this.setState({ showEditForm: false });
+
+      fetch(this.props.URL, {
+        method: 'POST', mode: 'cors', cache: 'no-cache',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(this.state.form)})
+      .then(response => response.json())
+      .then(data => {
+            this.setState({ 
+                contents: data['contents']
+            })
+            this.list.forceUpdateGrid();
+        });
+
+      event.preventDefault();
+    }
+
+    render() {
+        return (<div>
+             <Button 
+                variant="primary" 
+                onClick={() => this.setState({ 
+                  form: this.props.initForm(),
+                  showEditForm: true })}>
+                Add {this.props.pageName}
+            </Button>
+            
+            { this.state.showEditForm && this.generateForm() }
+            
+            <AutoSizer disableHeight>
+            {({width}) => (
+                <List
+                    ref={ref => { this.list = ref }}
+                    className="List"
+                    height={1500}
+                    overscanRowCount={10}
+                    rowCount={this.state.contents.length}
+                    rowHeight={50}
+                    rowRenderer={this.renderRow}
+                    width={width}
+                />
+            )}
+            </AutoSizer>
+            </div>
+        );
+    }
+
+    
+
+    renderRow({index, key, style}) {
+        const datum = this.state.contents[index];
+        
+        let loadForm = () => this.setState({
+            form: {
+                ...this.state.form,
+                ...datum,
+                ...this.props.populateForm(datum, this.state) },
+            showEditForm: true });
+        
+        return (
+          <div className="row" key={key} style={style} onClick={loadForm}>
+            <div>{JSON.stringify(datum)}</div>
+          </div>
+        );
+      }
+}
