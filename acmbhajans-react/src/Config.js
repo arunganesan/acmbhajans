4d5eedb5604ac59693c938beb68b5cd4093835cd
Arun Ganesan <arun@li351-93.members.linode.com>
2019-09-08 20:10:36 +0000

    added production base rails url

--- acmbhajans-react/src/Config.js
+++ acmbhajans-react/src/Config.js
@@ -1,4 +1,5 @@
 // export const URLBASE = 'http://localhost:1234';
 
 export const URLBASE = 'http://ammacentermichigan.com:1234';
+// export const URLBASE = 'https://ammacentermichigan.com/rails';
 export const BASEDIR = 'bhajans';
